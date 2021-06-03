7d3b8bbddae9dfef6acbbd19baf2fc20e84cbbb2
Anwesh43 <anweshthecool0@gmail.com>
2021-06-03 19:53:30 +0530

    adding Canvas extension methods

--- carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
+++ carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
     Color.parseColor(it)
 }.toTypedArray()
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.9f
+val sizeFactor : Float = 8.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val parts : Int = 3
@@ -28,3 +28,34 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawCarryBoxWithLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(0f, (h - size / 3) * sf2)
+    save()
+    translate((w - size) * (1 - sf4), 0f)
+    drawRect(RectF(size - size * sf1, 0f, size / 3, size), paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        translate(0f, size / 10 + (h - size / 5) * j)
+        drawLine(0f, 0f, (w - size) * (sf3 - sf4), 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCBWLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCarryBoxWithLine(scale, w, h, paint)
+}
