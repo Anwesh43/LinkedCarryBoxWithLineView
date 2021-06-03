1f2b20f9e512604889965a81c8f5d46def146cb3
Anwesh43 <anweshthecool0@gmail.com>
2021-06-03 17:05:30 +0530

    adding constants and libraries

--- carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
+++ carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
@@ -1,2 +1,25 @@
 package com.example.carryboxlwithlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
