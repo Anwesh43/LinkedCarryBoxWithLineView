0d44cbb564413280e556c2d54b4d07e459c6d737
Anwesh43 <anweshthecool0@gmail.com>
2021-06-03 20:55:38 +0530

    adding CBWLNode class

--- carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
+++ carryboxlwithlineview/src/main/java/com/example/carryboxlwithlineview/CarryBoxWithLineView.kt
@@ -123,4 +123,45 @@ class CarryBoxWithLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CBWLNode(var i : Int, val state : State = State()) {
+
+        private var next : CBWLNode? = null
+        private var prev : CBWLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CBWLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCBWLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CBWLNode {
+            var curr : CBWLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
